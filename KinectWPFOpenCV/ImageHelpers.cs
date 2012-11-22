8a1f74a2ca3a311b217cfd70d96b360571465f12
Erik Klimczak <eklimcz@gmail.com>
2012-11-21 21:12:43 -0600

    Initial Commit

---
+++ KinectWPFOpenCV/ImageHelpers.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Emgu.CV;
+
+namespace KinectWPFOpenCV
+{
+    public static class ImageHelpers
+    {
+       
+        private const int MaxDepthDistance = 4000;
+        private const int MinDepthDistance = 850;
+        private const int MaxDepthDistanceOffset = 3150;
+
+        public static BitmapSource SliceDepthImage(this DepthImageFrame image, int min = 20, int max = 1000)
+        {
+            int width = image.Width;
+            int height = image.Height;
+
+            //var depthFrame = image.Image.Bits;
+            short[] rawDepthData = new short[image.PixelDataLength];
+            image.CopyPixelDataTo(rawDepthData);
+           
+            var pixels = new byte[height * width * 4];
+          
+            const int BlueIndex = 0;
+            const int GreenIndex = 1;
+            const int RedIndex = 2;
+
+            for (int depthIndex = 0, colorIndex = 0;
+                depthIndex < rawDepthData.Length && colorIndex < pixels.Length;
+                depthIndex++, colorIndex += 4)
+            {
+
+                // Calculate the distance represented by the two depth bytes
+                int depth = rawDepthData[depthIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+
+                // Map the distance to an intesity that can be represented in RGB
+                var intensity = CalculateIntensityFromDistance(depth);
+
+                if (depth > min && depth < max)
+                {
+                    // Apply the intensity to the color channels
+                    pixels[colorIndex + BlueIndex] = intensity; //blue
+                    pixels[colorIndex + GreenIndex] = intensity; //green
+                    pixels[colorIndex + RedIndex] = intensity; //red                    
+                }
+            }
+
+            return BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, pixels, width * 4);
+        }
+
+        public static byte CalculateIntensityFromDistance(int distance)
+        {
+            // This will map a distance value to a 0 - 255 range
+            // for the purposes of applying the resulting value
+            // to RGB pixels.
+            int newMax = distance - MinDepthDistance;
+            if (newMax > 0)
+                return (byte)(255 - (255 * newMax
+                / (MaxDepthDistanceOffset)));
+            else
+                return (byte)255;
+        }
+
+
+        public static System.Drawing.Bitmap ToBitmap(this BitmapSource bitmapsource)
+        {
+            System.Drawing.Bitmap bitmap;
+            using (var outStream = new MemoryStream())
+            {
+                // from System.Media.BitmapImage to System.Drawing.Bitmap
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new System.Drawing.Bitmap(outStream);
+                return bitmap;
+            }
+        }
+
+
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        /// <summary>
+        /// Convert an IImage to a WPF BitmapSource. The result can be used in the Set Property of Image.Source
+        /// </summary>
+        /// <param name="image">The Emgu CV Image</param>
+        /// <returns>The equivalent BitmapSource</returns>
+        public static BitmapSource ToBitmapSource(IImage image)
+        {
+            using (System.Drawing.Bitmap source = image.Bitmap)
+            {
+                IntPtr ptr = source.GetHbitmap(); //obtain the Hbitmap
+
+                BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    ptr,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                DeleteObject(ptr); //release the HBitmap
+                return bs;
+            }
+        }
+
+    }
+}
---
+++ KinectWPFOpenCV/MainWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using Microsoft.Kinect;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.IO;
+
+namespace KinectWPFOpenCV
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor sensor;
+        WriteableBitmap depthBitmap;
+        WriteableBitmap colorBitmap;
+        DepthImagePixel[] depthPixels;
+        byte[] colorPixels;
+
+        int blobCount = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            this.Loaded += MainWindow_Loaded;
+            this.Closing += MainWindow_Closing;
+            this.MouseDown += MainWindow_MouseDown;
+
+        }
+
+      
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+
+            if (null != this.sensor)
+            {
+
+                this.sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+                this.depthPixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
+                this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+                this.depthBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);                
+                this.colorImg.Source = this.colorBitmap;
+
+                this.sensor.AllFramesReady += this.sensor_AllFramesReady;
+
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+
+            if (null == this.sensor)
+            {
+                this.outputViewbox.Visibility = System.Windows.Visibility.Collapsed;
+                this.txtError.Visibility = System.Windows.Visibility.Visible;
+                this.txtInfo.Text = "No Kinect Found";
+                
+            }
+
+        }
+
+        private void sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            BitmapSource depthBmp = null;
+            blobCount = 0;
+
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+                {
+                    if (depthFrame != null)
+                    {
+
+                        blobCount = 0;
+
+                        depthBmp = depthFrame.SliceDepthImage((int)sliderMin.Value, (int)sliderMax.Value);
+                        
+                        Image<Bgr, Byte> openCVImg = new Image<Bgr, byte>(depthBmp.ToBitmap());
+                        Image<Gray, byte> gray_image = openCVImg.Convert<Gray, byte>();
+
+                        using (MemStorage stor = new MemStorage())
+                        {
+                            //Find contours with no holes try CV_RETR_EXTERNAL to find holes
+                            Contour<System.Drawing.Point> contours = gray_image.FindContours(
+                             Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE,
+                             Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_EXTERNAL,
+                             stor);
+
+                            for (int i = 0; contours != null; contours = contours.HNext)
+                            {
+                                i++;
+
+                                if ((contours.Area > Math.Pow(sliderMinSize.Value, 2)) && (contours.Area < Math.Pow(sliderMaxSize.Value, 2)))
+                                {
+                                    MCvBox2D box = contours.GetMinAreaRect();                                    
+                                    openCVImg.Draw(box, new Bgr(System.Drawing.Color.Red), 2);                                    
+                                    blobCount++;
+                                }
+                            }
+                        }
+
+                        this.outImg.Source = ImageHelpers.ToBitmapSource(openCVImg);                        
+                        txtBlobCount.Text = blobCount.ToString();
+                    }
+                }
+
+
+                if (colorFrame != null)
+                {
+                    
+                      colorFrame.CopyPixelDataTo(this.colorPixels);
+                      this.colorBitmap.WritePixels(
+                          new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                          this.colorPixels,
+                          this.colorBitmap.PixelWidth * sizeof(int),
+                          0);
+                    
+                }
+            }
+        }
+
+
+        #region Window Stuff
+        void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+
+        void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        private void CloseBtnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+    }
+}
